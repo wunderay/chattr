92c15936e995feda79605a52704bf7f904b12a75
Wunderay <60483832+wunderay@users.noreply.github.com>
2021-04-14 18:28:10 -0600

    Added layout and signup

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const express= require("express"), app = express();
+const express= require("express"), app = express(),
+mongoose = require("mongoose"), router = express.Router(),
+methodOverride = require("method-override");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine", {useNewUrlParser: true});
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+router.use(express.static("public"));
+
+router.use(methodOverride("_method", {methods: ["POST", "GET"]}));
